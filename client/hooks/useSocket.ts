9d216142b9fc5ba7d210a003df4ceadf83842291
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-04-25 15:04:09 +0500

    Update useSocket.ts

--- client/hooks/useSocket.ts
+++ client/hooks/useSocket.ts
@@ -8,45 +8,15 @@ export function useSocket() {
   const [isConnected, setIsConnected] = useState(false);
 
   useEffect(() => {
-    const socketInstance = io(process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:4000', {
-      transports: ['websocket'],
-      autoConnect: true,
-    });
-
-    socketInstance.on('connect', () => {
-      console.log('Socket connected');
-      setIsConnected(true);
-    });
-
-    socketInstance.on('disconnect', () => {
-      console.log('Socket disconnected');
-      setIsConnected(false);
-    });
-
+    const socketInstance = io(process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:4000');
+    
+    socketInstance.on('connect', () => setIsConnected(true));
+    socketInstance.on('disconnect', () => setIsConnected(false));
+    
     setSocket(socketInstance);
-
-    return () => {
-      socketInstance.close();
-    };
+    
+    return () => { socketInstance.close(); };
   }, []);
 
-  const emit = (event: string, data: any) => {
-    if (socket && isConnected) {
-      socket.emit(event, data);
-    }
-  };
-
-  const on = (event: string, callback: (data: any) => void) => {
-    if (socket) {
-      socket.on(event, callback);
-    }
-  };
-
-  const off = (event: string, callback?: (data: any) => void) => {
-    if (socket) {
-      socket.off(event, callback);
-    }
-  };
-
-  return { socket, isConnected, emit, on, off };
+  return { socket, isConnected };
 }
