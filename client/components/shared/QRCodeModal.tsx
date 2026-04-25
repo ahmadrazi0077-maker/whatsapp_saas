71d6a44f61df1e4b542813fefad390c7a5fa9f60
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-04-26 00:33:47 +0500

    Update QRCodeModal.tsx

--- client/components/shared/QRCodeModal.tsx
+++ client/components/shared/QRCodeModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 
