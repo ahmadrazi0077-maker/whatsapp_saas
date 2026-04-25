a6a9a6e6f197eb6c2bab9e2123aaf4e557695bf1
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-04-25 13:17:58 +0500

    Add files via upload

---
+++ client/hooks/useSocket.ts
@@ -0,0 +1,52 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+
+export function useSocket() {
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    const socketInstance = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
+      transports: ['websocket'],
+      autoConnect: true,
+    });
+
+    socketInstance.on('connect', () => {
+      console.log('Socket connected');
+      setIsConnected(true);
+    });
+
+    socketInstance.on('disconnect', () => {
+      console.log('Socket disconnected');
+      setIsConnected(false);
+    });
+
+    setSocket(socketInstance);
+
+    return () => {
+      socketInstance.close();
+    };
+  }, []);
+
+  const emit = (event: string, data: any) => {
+    if (socket && isConnected) {
+      socket.emit(event, data);
+    }
+  };
+
+  const on = (event: string, callback: (data: any) => void) => {
+    if (socket) {
+      socket.on(event, callback);
+    }
+  };
+
+  const off = (event: string, callback?: (data: any) => void) => {
+    if (socket) {
+      socket.off(event, callback);
+    }
+  };
+
+  return { socket, isConnected, emit, on, off };
+}
