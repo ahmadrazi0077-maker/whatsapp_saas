7779f53c30cec8f4c7d712feb97c8462b54407b2
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-04-27 11:48:17 +0500

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['localhost', 'avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
