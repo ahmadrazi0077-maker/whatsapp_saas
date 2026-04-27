aed7b5c2fb95db2ca4fc649e62e73b9d830b2e49
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-04-27 10:59:33 +0500

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  images: {
-    domains: ['localhost', 'supabase.co'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': require('path').resolve(__dirname, './'),
-    };
-    return config;
-  },
-  // Disable strict mode for now to avoid module issues
-  reactStrictMode: false,
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
