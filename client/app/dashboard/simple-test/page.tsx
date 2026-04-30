434e8cacbc29c956e7955b13e57c6b5ef47a8f98
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-04-30 17:59:52 +0500

    Delete client/app/dashboard/simple-test directory

--- client/app/dashboard/simple-test/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function SimpleTestPage() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>Test Page</h1>
-      <p>If you can see this, routing is working!</p>
-      <a href="/dashboard">Go to Dashboard</a>
-    </div>
-  );
-}
