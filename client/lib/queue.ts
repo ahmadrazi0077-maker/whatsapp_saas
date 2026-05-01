4bf6fcab56c0b4d24b8829d43bd86603a437cca5
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-05-01 19:22:56 +0500

    Delete client/lib/queue.ts

--- client/lib/queue.ts
+++
@@ -1,9 +0,0 @@
-import { Queue, Worker } from 'bullmq';
-import { redis } from './redis';
-
-export const broadcastQueue = new Queue('broadcast', { connection: redis });
-
-export const broadcastWorker = new Worker('broadcast', async (job) => {
-  const { campaignId, message, contacts } = job.data;
-  // Process broadcast
-}, { connection: redis });
