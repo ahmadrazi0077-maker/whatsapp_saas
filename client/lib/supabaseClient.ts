e69b6d93f6426adeb9ee573efb050d178ed8edab
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-05-01 13:25:48 +0500

    Update supabaseClient.ts

--- client/lib/supabaseClient.ts
+++ client/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://xsxtbztyqjmlwfnibtdm.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'your-anon-key'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_e-Q5WGbgtfRRuQJr3NO6gw_972Ok9Jy'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
