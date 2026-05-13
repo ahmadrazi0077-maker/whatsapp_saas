058767e66b7369b947ac907aa35e36b0b651c680
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-05-13 15:30:09 +0500

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: 'standalone',
+  experimental: {
+    // Skip static generation for all pages
+  },
 }
 
 module.exports = nextConfig
