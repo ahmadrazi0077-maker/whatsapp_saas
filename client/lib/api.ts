aa8b54bc21af35257c4787cf4e28bf99836fab12
ahmadrazi0077-maker <ahmadrazi0077@gmail.com>
2026-05-15 00:52:01 +0500

    Update api.ts

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = '${process.env.NEXT_PUBLIC_API_URL}';
+const API_BASE_URL = '/api';
 
 async function request<T>(endpoint: string, options: any = {}): Promise<T> {
   const { method = 'GET', body } = options;
